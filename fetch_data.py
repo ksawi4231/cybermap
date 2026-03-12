215e9b452ab2557316f7db4656b3b91e32845948
ksawi4231 <84082739+ksawi4231@users.noreply.github.com>
2026-03-12 19:55:04 +0100

    Refactor fetch_data.py to include API requests
    zmiana

--- fetch_data.py
+++ fetch_data.py
@@ -1,45 +1,54 @@
 import requests
 import json
 import random
+import time
 
 wszystkie_ataki = []
 wszytkie_miasta = []
+lista_odpowiedzi=[]
+x=[]
+y=[]
 
-with open('testowe_ip.json', 'r') as file1:
-    data = json.load(file1)
-
-with open('miasta_lokalizacja.json', 'r') as file2:
-    miasta = json.load(file2)
+with open('miasta_lokalizacja.json', 'r') as file1:
+    miasta = json.load(file1)
 
 for i in miasta['cities']:
     wszytkie_miasta.append(i)
 
-#url = 'https://api.abuseipdb.com/api/v2/blacklist'
+url = 'https://api.abuseipdb.com/api/v2/blacklist'
 
-#querystring = {'confidenceMinimum':'90'}
+querystring = {'confidenceMinimum':'90', 'limit': '15'}
 
-#headers = {
-    #'Accept': 'application/json',
-   #'Key': '85a3db26ce572ebf950ec258f5d36a9f46e38899fa62c85e00ae8b0c95141c65289ef164e3c251b0'
-#}
+headers = {
+    'Accept': 'application/json',
+    'Key': '85a3db26ce572ebf950ec258f5d36a9f46e38899fa62c85e00ae8b0c95141c65289ef164e3c251b0'
+}
 
-# = requests.request(method='GET', url=url, headers=headers, params=querystring)
+baza_danych = requests.request(method='GET', url=url, headers=headers, params=querystring).json()
 
-#dane = response.json()
+for i in baza_danych['data']:
+    odpowiedz = requests.get(f"http://ip-api.com/json/{i['ipAddress']}")
+    lista_odpowiedzi.append(odpowiedz.json())
+    time.sleep(1.5)
 
-for i in data['data']:
-    ip=i['ipAddress']
-    lokalizacja = requests.get(f'http://ip-api.com/json/{ip}').json()
-    if lokalizacja['status'] == 'success':
-        cel = random.choice(wszytkie_miasta)
-        atak = {
-            "startLat": lokalizacja['lat'],
-            "startLng": lokalizacja['lon'],
-            "endLat": cel['lat'],
-            "endLng": cel['lng'],
-            "ip": ip,
-        }
-        wszystkie_ataki.append(atak)
+
+for i in lista_odpowiedzi:
+    x.append(i['lat'])
+    y.append(i['lon'])
+
+licznik=0
+
+for i in baza_danych['data']:
+    
+    cel = random.choice(wszytkie_miasta)
+    atak = {
+        "startLat": x[licznik],
+        "startLng": y[licznik],
+        "endLat": cel['lat'],
+        "endLng": cel['lng'],
+    }
+    wszystkie_ataki.append(atak)
+    licznik+=1
 
 with open('attacks.json', 'w') as f:
     json.dump(wszystkie_ataki, f, indent=4)
